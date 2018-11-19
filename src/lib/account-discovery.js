af3b3b67d9f1576e516fb6c1ac93c2d7a655c3a6
Luke Childs <lukechilds123@gmail.com>
2018-11-19 19:34:11 +0700

    Implement basic BIP44 account discovery

---
+++ src/lib/account-discovery.js
@@ -0,0 +1,33 @@
+import getLedger from './get-ledger';
+
+
+const accountDiscovery = async () => {
+  const insightUrl = 'https://kmdexplorer.io/insight-api-komodo';
+  const addresses = [];
+  const gapLimit = 20;
+  let consecutiveUnusedAddresses = 0;
+  let currentAddress = 0;
+
+  const ledger = await getLedger();
+
+  while (consecutiveUnusedAddresses < gapLimit) {
+    const pubKey = await ledger.getWalletPublicKey(`44'/141'/0'/0/${currentAddress}`);
+    const address = await (await fetch(`${insightUrl}/addr/${pubKey.bitcoinAddress}/?noTxList=1`)).json();
+
+    addresses.push(address);
+
+    if(address.totalReceived > 0 || address.unconfirmedBalance > 0) {
+      consecutiveUnusedAddresses = 0;
+    } else {
+      consecutiveUnusedAddresses++;
+    }
+
+    currentAddress++;
+  }
+
+  await ledger.close();
+
+  return addresses.slice(0, addresses.length - gapLimit);
+};
+
+export default accountDiscovery;
