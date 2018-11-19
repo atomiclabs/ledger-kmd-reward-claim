d90e8dc5386b14bf1f0435daf91ac9d7b07daa83
Luke Childs <lukechilds123@gmail.com>
2018-11-19 19:34:11 +0700

    Implement basic BIP44 account discovery

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import accountDiscovery from './lib/account-discovery';
 import './App.css';
 
-const App = () => (
-	<div className="App">
-    Hello World
-  </div>
-);
+class App extends React.Component {
+  state = {
+    scanning: false,
+    addresses: []
+  };
+
+  scanAddresses = async () => {
+    this.setState({scanning: true});
+
+    const addresses = await accountDiscovery();
+
+    this.setState({
+      scanning: false,
+      addresses
+    });
+  };
+
+  render() {
+    const {addresses, scanning} = this.state;
+    return (
+      <div className="App">
+        <button onClick={() => this.scanAddresses()}>
+          Scan Blockchain for Addresses
+        </button>
+        <div>
+          {scanning && 'Scanning...'}
+        </div>
+        {addresses.map(address => (
+          <pre key={address.addrStr}>{JSON.stringify(address)}</pre>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default App;
