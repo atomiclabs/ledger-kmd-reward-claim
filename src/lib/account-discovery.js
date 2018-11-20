87c1c973832fad22d8db951ceb58711faf52c72b
Luke Childs <lukechilds123@gmail.com>
2018-11-20 11:11:03 +0700

    Implement full BIP44 account discovery

--- src/lib/account-discovery.js
+++ src/lib/account-discovery.js
@@ -1,34 +1,54 @@
 import getLedger from './get-ledger';
 
-
-const accountDiscovery = async () => {
+const walkDerivationPath = async ({account, isChange}) => {
   const insightUrl = 'https://kmdexplorer.io/insight-api-komodo';
   const addresses = [];
   const gapLimit = 20;
   let consecutiveUnusedAddresses = 0;
-  let currentAddress = 0;
+  let addressIndex = 0;
 
   const ledger = await getLedger();
 
   while (consecutiveUnusedAddresses < gapLimit) {
-    const derivationPath = `44'/141'/0'/0/${currentAddress}`;
+    const derivationPath = `44'/141'/${account}'/${isChange ? 1 : 0}/${addressIndex}`;
     const pubKey = await ledger.getWalletPublicKey(derivationPath);
     const address = await (await fetch(`${insightUrl}/addr/${pubKey.bitcoinAddress}/?noTxList=1`)).json();
 
-    addresses.push({derivationPath, ...address});
+    addresses.push({account, isChange, addressIndex, derivationPath, ...address});
 
-    if(address.totalReceived > 0 || address.unconfirmedBalance > 0) {
+    if (address.totalReceived > 0 || address.unconfirmedBalance > 0) {
       consecutiveUnusedAddresses = 0;
     } else {
       consecutiveUnusedAddresses++;
     }
 
-    currentAddress++;
+    addressIndex++;
   }
 
   await ledger.close();
 
   return addresses.slice(0, addresses.length - gapLimit);
+};
+
+const getAccountAddresses = async account => [
+  ...await walkDerivationPath({account, isChange: false}),
+  ...await walkDerivationPath({account, isChange: true})
+];
+
+const accountDiscovery = async () => {
+  let addresses = [];
+  let account = 0;
+
+  while (true) {
+    const accountAddresses = await getAccountAddresses(account);
+    if (accountAddresses.length === 0) {
+      break;
+    }
+    addresses = [...addresses, ...accountAddresses];
+    account++;
+  }
+
+  return addresses;
 };
 
 export default accountDiscovery;
