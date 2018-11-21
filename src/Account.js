27add6be288086b50f57d5cc3de7bd822b05d83e
Luke Childs <lukechilds123@gmail.com>
2018-11-21 10:33:31 +0700

    Show account balance

--- src/Account.js
+++ src/Account.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Account = ({account, utxos}) => {
+  const balance = utxos.reduce((balance, utxo) => balance + utxo.amount, 0);
+
   return (
     <div className="Account">
-      <h2>Account {account + 1}</h2>
+      <h2>Account {account + 1}: {balance} KMD</h2>
       {utxos.map(utxo => (
         <pre key={JSON.stringify(utxo)}>{JSON.stringify(utxo, null, 2)}</pre>
       ))}
