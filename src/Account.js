c243aa0638466ad7eccafbed13967ff7d8246411
Luke Childs <lukechilds123@gmail.com>
2018-11-29 11:36:50 +0700

    Show account total rewards and service fee

--- src/Account.js
+++ src/Account.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import getKomodoRewards from './lib/get-komodo-rewards';
+import {SERVICE_FEE_PERCENT} from './constants'
 import {toBitcoin} from 'satoshi-bitcoin';
 
 const Account = ({account, tiptime, utxos}) => {
   const balance = utxos.reduce((balance, utxo) => balance + utxo.satoshis, 0);
+  const rewards = utxos.reduce((rewards, utxo) => rewards + getKomodoRewards({tiptime, ...utxo}), 0);
+  const fee = Math.floor((rewards / 100) * SERVICE_FEE_PERCENT);
 
   return (
     <div className="Account">
       <h2>Account {account + 1}: {toBitcoin(balance)} KMD</h2>
+      <h4>Total rewards are {toBitcoin(rewards)} KMD with a {SERVICE_FEE_PERCENT}% fee of {toBitcoin(fee)} KMD meaning you will be credited with an extra {toBitcoin(rewards - fee)} KMD</h4>
       {utxos.map(utxo => (
         <div key={utxo.id} className="UTXO">
           <h3>{utxo.address}</h3>
