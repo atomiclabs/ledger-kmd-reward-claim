84473468e311d1e0359b67d5c5e446046ccf7cf4
Luke Childs <lukechilds123@gmail.com>
2018-12-10 18:21:26 +0700

    Improve UTXO table appearance

--- src/Utxos.js
+++ src/Utxos.js
@@ -2,27 +2,33 @@ import React from 'react';
 import {toBitcoin} from 'satoshi-bitcoin';
 import getKomodoRewards from './lib/get-komodo-rewards';
 
-const Utxos = ({utxos, tiptime}) => (
-  <table className="table">
-    <thead>
-      <tr>
-        <th>Address</th>
-        <th>Balance</th>
-        <th>Locktime</th>
-        <th>Rewards</th>
-      </tr>
-    </thead>
-    <tbody>
-      {utxos.map(utxo => (
-        <tr key={utxo.id} className="utxo">
-          <th>{utxo.address}</th>
-          <td>{toBitcoin(utxo.satoshis)}</td>
-          <td>{utxo.locktime}</td>
-          <td>{toBitcoin(getKomodoRewards({tiptime, ...utxo}))}</td>
+const Utxos = ({utxos, tiptime}) => {
+  const headings = ['Address', 'Balance', 'Locktime', 'Rewards'];
+
+  return (
+    <table className="table is-striped">
+      <thead>
+        <tr>
+          {headings.map(heading => <th key={heading}>{heading}</th>)}
         </tr>
-      ))}
-    </tbody>
-  </table>
-);
+      </thead>
+      <tfoot>
+        <tr>
+          {headings.map(heading => <th key={heading}>{heading}</th>)}
+        </tr>
+      </tfoot>
+      <tbody>
+        {utxos.map(utxo => (
+          <tr key={utxo.id} className="utxo">
+            <th>{utxo.address}</th>
+            <td>{toBitcoin(utxo.satoshis)}</td>
+            <td>{utxo.locktime}</td>
+            <td>{toBitcoin(getKomodoRewards({tiptime, ...utxo}))}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default Utxos;
