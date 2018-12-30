70ba345dd7c8bbdaf7bebb22d3f71559aa8dde94
Luke Childs <lukechilds123@gmail.com>
2018-12-30 10:17:27 +0700

    Compile custom bulma styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '@fortawesome/fontawesome-free/css/all.css';
-import 'bulma/css/bulma.css';
+import './bulma-custom.scss';
 import './index.css';
 import App from './App';
 
