20a222c545877f8a51fa242e2ba2d9b611d485e7
Luke Childs <lukechilds123@gmail.com>
2019-01-03 14:43:22 +0700

    Destructure account data for readability

--- src/Accounts.js
+++ src/Accounts.js
@@ -10,22 +10,29 @@ import './Account.scss';
 
 class Account extends React.Component {
   claimRewards = async () => {
-    const {account} = this.props;
+    const {
+      addresses,
+      utxos,
+      balance,
+      claimableAmount,
+      serviceFee,
+      externalNode
+    } = this.props.account;
 
-    const unusedAddressIndex = account.addresses.filter(address => !address.isChange).length;
-    const unusedAddress = getAddress(account.externalNode.derive(unusedAddressIndex).publicKey);
+    const unusedAddressIndex = addresses.filter(address => !address.isChange).length;
+    const unusedAddress = getAddress(externalNode.derive(unusedAddressIndex).publicKey);
 
     console.log(unusedAddress);
 
     const outputs = [
-      {address: unusedAddress, value: (account.balance + account.claimableAmount)}
+      {address: unusedAddress, value: (balance + claimableAmount)}
     ];
 
-    if (account.serviceFee > 0) {
-      outputs.push({address: SERVICE_FEE_ADDRESS, value: account.serviceFee})
+    if (serviceFee > 0) {
+      outputs.push({address: SERVICE_FEE_ADDRESS, value: serviceFee})
     }
 
-    const rewardClaimTransaction = await ledger.createTransaction(account.utxos, outputs);
+    const rewardClaimTransaction = await ledger.createTransaction(utxos, outputs);
     console.log(rewardClaimTransaction);
 
     const result = await blockchain.broadcast(rewardClaimTransaction);
@@ -34,7 +41,15 @@ class Account extends React.Component {
 
   render() {
     const {accountIndex, account, tiptime} = this.props;
-    const isClaimableAmount = (account.claimableAmount > 0);
+    const {
+      utxos,
+      balance,
+      rewards,
+      claimableAmount,
+      serviceFee
+    } = account;
+
+    const isClaimableAmount = (claimableAmount > 0);
 
     return (
       <div className="Account column is-full">
@@ -43,25 +58,25 @@ class Account extends React.Component {
             <h2>
               Account {accountIndex + 1}
               <div className="balance">
-                {toBitcoin(account.balance)} KMD
+                {toBitcoin(balance)} KMD
               </div>
               <small>
-                + {toBitcoin(Math.max(0, account.claimableAmount))} KMD Claimable Rewards
+                + {toBitcoin(Math.max(0, claimableAmount))} KMD Claimable Rewards
               </small>
             </h2>
             <h4>UTXOs</h4>
-            <Utxos utxos={account.utxos} tiptime={tiptime} />
+            <Utxos utxos={utxos} tiptime={tiptime} />
             {isClaimableAmount && (
               <>
                 <h4>Breakdown</h4>
                 <table className="breakdown">
                   <tbody>
                     <tr>
-                      <td>{toBitcoin(account.rewards)} KMD</td>
+                      <td>{toBitcoin(rewards)} KMD</td>
                       <td>Rewards accrued</td>
                     </tr>
                     <tr>
-                      <td>{toBitcoin(account.serviceFee)} KMD</td>
+                      <td>{toBitcoin(serviceFee)} KMD</td>
                       <td>{SERVICE_FEE_PERCENT}% service fee</td>
                     </tr>
                     <tr>
@@ -69,7 +84,7 @@ class Account extends React.Component {
                       <td>Network transaction fee</td>
                     </tr>
                     <tr>
-                      <td><strong>{toBitcoin(account.claimableAmount)} KMD</strong></td>
+                      <td><strong>{toBitcoin(claimableAmount)} KMD</strong></td>
                       <td>Total claimable amount</td>
                     </tr>
                   </tbody>
